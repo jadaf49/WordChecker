937e50cc51d6b077b408557a0de3076fb55e5b92
jadaf49 <jadaf49@nycstudents.net>
2024-10-09 16:32:44 +0000

    set up

---
+++ WordChecker.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class WordChecker
+{
+    public WordChecker(ArrayList<String> list) //overload the constructors so that the zero-parameter constructor instantiates an empty ArrayList.
+
+    {
+        wordList=list;
+    }
+}
