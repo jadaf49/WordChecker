cad8262f71e4097078ed188fcb2e85986732b891
jadaf49 <jadaf49@nycstudents.net>
2024-10-16 16:21:58 +0000

    testing program

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
 public class Main
 {
     public static void main (String[]args)
     {
         WordChecker w = new WordChecker();
+        //System.out.print(w);
+        ArrayList<String>words=new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        System.out.print(words);
+
+        
+        words= words.isWordChain();
+        System.out.print(words);
+
     }
 }
--- WordChecker.java
+++ WordChecker.java
@@ -1,9 +1,26 @@
 import java.util.ArrayList;
 public class WordChecker
 {
+    private ArrayList<String>wordList;
     public WordChecker(ArrayList<String> list) //overload the constructors so that the zero-parameter constructor instantiates an empty ArrayList.
 
     {
         wordList=list;
+    }
+    public WordChecker()
+    {
+        wordList = new ArrayList<String>();
+    }
+    public boolean isWordChain()
+    {
+        boolean wordChain = true;
+        for(int i = 1; i < wordList.size(); i++)
+        {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if(after.indexOf(before) < 0) return false;
+        }
+        return true;
+        
     }
 }
