080d66b3081d039195d881fd63533a5a1065612a
Jack-Fives <jackf58@nycstudents.net>
2024-10-24 12:06:34 -0400

    Update WordChecker.java

--- WordChecker.java
+++ WordChecker.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 public class WordChecker
 {
     private ArrayList<String>wordList;
+    
     public WordChecker(ArrayList<String> list) //overload the constructors so that the zero-parameter constructor instantiates an empty ArrayList.
 
     {
@@ -20,7 +21,20 @@ public class WordChecker
             String before = wordList.get(i - 1);
             if(after.indexOf(before) < 0) return false;
         }
+        
         return true;
         
     }
+    public ArrayList<String> createList(String target) {
+        ArrayList <String> list = new ArrayList<String>();
+    
+        for(String s : wordList){
+            if(s.indexOf(target)==0){
+                list.add(s.substring(target.length()));
+            }
+        }
+        return list;
+    }
+    
+     
 }
